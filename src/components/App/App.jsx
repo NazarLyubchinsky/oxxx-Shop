b1d47dc5a31df3a30a5114b46c98cad6838131ce
Nazar Lyubchinsky <nazar.muxal@gmail.com>
2025-08-01 11:12:12 +0300

    щось пішло не поплану

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,5 @@
-import React, { Suspense } from 'react'
-import '../../styles/index.scss'
-
-
-// components
-import Footer from '../Footer/Footer'
-import Header from '../Header/Header'
-import AppRoutes from './AppRoutes'
-import Preloader from '../Preloader/Preloader'
-import { useLocation } from 'react-router-dom'
-
-const App = () => {
-	const location = useLocation()
-	return (
-		<Suspense fallback={<Preloader />} className='app'>
-			<Header />
-			<AppRoutes />
-			{
-				location.key === 'default' ?  '' : <Footer />
-			}
-			
-		</Suspense>
-	)
+function App() {
+  return <div>Hello world</div>;
 }
 
-export default App
+export default App;
